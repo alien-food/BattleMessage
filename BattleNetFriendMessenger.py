f3e3e5e6090edf4826e0f0775d22749b748dff6d
alien-food <107591346+alien-food@users.noreply.github.com>
2022-08-06 22:46:20 -0500

    Update BattleNetFriendMessenger.py
    Used OOP to give the BattleMessenger attributes and functions.

--- BattleNetFriendMessenger.py
+++ BattleNetFriendMessenger.py
@@ -4,42 +4,54 @@ import time
 import win32gui
 import win32con
 
-name = str(input("Who would you like to message? "))
-msg = str(input("What would you like to say? "))
-fav = str(input("Is this friend in your favorites? ")).lower()
 
-# Start up Battle.net, you need to already be signed in
-Application().start(r"C:\\Program Files (x86)\\Battle.net\\Battle.net.exe")
-time.sleep(3)
+class Messenger:
+    def __init__(self, pathy: str):
+        self.pathy = pathy
+        
 
-# Find our Window and Maximize it for proper calibration
-bnetWindow = win32gui.GetForegroundWindow()
-win32gui.ShowWindow(bnetWindow, win32con.SW_MAXIMIZE)
-width, height = pyautogui.size()
-# print("width: " + str(width))
-# print("height: " + str(height))
-x = width * .9375
-y = height * .1018
+    def questions(self):
+        self.name = str(input("Who would you like to message? "))
+        self.msg = str(input("What would you like to say? "))
+        self.fav = str(input("Is this friend in your favorites? ")).lower()
+        
 
-# Navigate to the Friend Search bar
-pyautogui.moveTo(x, y, duration=1)
-pyautogui.click()
+    def starterup(self):
+        Application().start(self.pathy)
+        time.sleep(5)
+        bnetWindow = win32gui.GetForegroundWindow()
+        win32gui.ShowWindow(bnetWindow, win32con.SW_MAXIMIZE)
+        width, height = pyautogui.size()
+        x = width * .9375
+        y = height * .1018
+        
+        # Navigate to the Friend Search bar
+        pyautogui.moveTo(x, y, duration=1)
+        pyautogui.click()
 
-# Search for name and type them a message
-for letter in name:
-    pyautogui.hotkey(letter)
-time.sleep(3)
 
-# Check if friend is in favorites
-if "y" in fav:
-    for i in range(3):
-        pyautogui.hotkey("tab")
-else:
-    for i in range(4):
-        pyautogui.hotkey("tab")
+    def BnetMessage(self):
+        # Search for name and type them a message
+        for letter in self.name:
+            pyautogui.hotkey(letter)
+        time.sleep(3)
 
-pyautogui.hotkey("enter")
-time.sleep(5) 
-for letter in msg:
-    pyautogui.hotkey(letter)
-pyautogui.hotkey("enter")
+        # Check if friend is in favorites
+        if "y" in self.fav:
+            for i in range(3):
+                pyautogui.hotkey("tab")
+        else:
+            for i in range(4):
+                pyautogui.hotkey("tab")
+
+        pyautogui.hotkey("enter")
+        time.sleep(5) 
+        for letter in self.msg:
+            pyautogui.hotkey(letter)
+        pyautogui.hotkey("enter")
+
+
+Test = Messenger(r"C:\\Program Files (x86)\\Battle.net\\Battle.net.exe")
+Test.questions()
+Test.starterup()
+Test.BnetMessage()
