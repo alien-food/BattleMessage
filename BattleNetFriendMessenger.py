02cdfda7b355b053dfa8c60f5bf5d3d3c59abf57
alien-food <107591346+alien-food@users.noreply.github.com>
2022-08-06 21:51:03 -0500

    Update BattleNetFriendMessenger.py

--- BattleNetFriendMessenger.py
+++ BattleNetFriendMessenger.py
@@ -1,13 +1,12 @@
-from pywinauto import Desktop, Application
-from pywinauto.keyboard import send_keys
+from pywinauto import Application
 import pyautogui
 import time
 import win32gui
 import win32con
 
-
 name = str(input("Who would you like to message? "))
 msg = str(input("What would you like to say? "))
+fav = str(input("Is this friend in your favorites? ")).lower()
 
 # Start up Battle.net, you need to already be signed in
 Application().start(r"C:\\Program Files (x86)\\Battle.net\\Battle.net.exe")
@@ -30,10 +29,17 @@ pyautogui.click()
 for letter in name:
     pyautogui.hotkey(letter)
 time.sleep(3)
-for i in range(4):
-    pyautogui.hotkey("tab")
+
+# Check if friend is in favorites
+if "y" in fav:
+    for i in range(3):
+        pyautogui.hotkey("tab")
+else:
+    for i in range(4):
+        pyautogui.hotkey("tab")
+
 pyautogui.hotkey("enter")
 time.sleep(5) 
 for letter in msg:
     pyautogui.hotkey(letter)
-
+pyautogui.hotkey("enter")
